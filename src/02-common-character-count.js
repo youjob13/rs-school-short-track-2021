6a6029e895bc2f0a929972335cf124f3f43d2092
Danil Rodionow <danil.rodionow13@gmail.com>
2021-04-09 21:51:53 +0300

    feat: add second task

--- src/02-common-character-count.js
+++ src/02-common-character-count.js
@@ -9,36 +9,33 @@
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  // const obj = {};
-  // let res = 0;
-  // let s3 = s2;
-  // s1.split('').forEach((letter) => {
-  //   if (s3.includes(letter)) {
-  //     obj[letter] = 0;
-  //     s3 = s3
-  //       .split('')
-  //       .filter((item) => {
-  //         if (item === letter) {
-  //           obj[letter]++;
-  //         } else {
-  //           return item;
-  //         }
-  //       })
-  //       .join('');
-  //     obj[letter] = Math.min(
-  //       obj[letter],
-  //       s1.split('').filter((item) => item === letter).length
-  //     );
-  //   }
-  // });
-
-  // for (const prop in obj) {
-  //   res += obj[prop];
-  // }
-
-  // return res;
-  throw new Error('Not implemented');
+function getCommonCharacterCount(s1, s2) {
+  const obj = {};
+  let res = 0;
+  let s3 = s2;
+  s1.split('').forEach((letter) => {
+    if (s3.includes(letter)) {
+      obj[letter] = 0;
+      s3 = s3
+        .split('')
+        .filter((item) => {
+          if (item === letter) {
+            obj[letter]++;
+          }
+          return item;
+        })
+        .join('');
+      obj[letter] = Math.min(
+        obj[letter],
+        s1.split('').filter((item) => item === letter).length,
+      );
+    }
+  });
+  const arr = Object.keys(obj);
+  for (let i = 0; i < arr.length; i++) {
+    res += obj[arr[i]];
+  }
+  return res;
 }
 
 module.exports = getCommonCharacterCount;
